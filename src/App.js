f70d719fd7b59734effa85c649b69239d5258698
Serenity1331 <saveyou13@gmail.com>
2021-07-13 16:12:55 +0300

    router edited

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import NotificationsPage from './pages/notifications-page/NotificationsPage'
 import NotificationsRecordPage from './pages/notifications-record-page/NotificationsRecordPage'
 
@@ -7,10 +7,8 @@ import NotificationsRecordPage from './pages/notifications-record-page/Notificat
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Route exact path="/" component={NotificationsPage} />
-        <Route exact path="/records" component={NotificationsRecordPage} />
-      </Router>
+      <Route exact path="/" component={NotificationsPage} />
+      <Route exact path="/records" component={NotificationsRecordPage} />
     </div>
   );
 }
